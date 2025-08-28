fae80982886f650a22076dfcd214ba3bab2f0cd1
GabrielCarv-Dev <224664621+GabrielCarv-Dev@users.noreply.github.com>
2025-08-28 17:14:54 -0300

    fix:correção na copia .env em dockerfile

--- api/db.py
+++ api/db.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASS = os.getenv("POSTGRES_PASSWORD")
-DB_HOST = os.getenv("POSTGRES_HOST", "db")
+DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
 DB_PORT = os.getenv("POSTGRES_PORT", "5432")
 DB_NAME = os.getenv("POSTGRES_DB")
 
